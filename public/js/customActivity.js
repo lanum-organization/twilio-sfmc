fcceff4b5b8839b1237fd63327bd4c6b45f943c7
Jonathan Tavares <jonathantavares.all@gmail.com>
2024-05-06 13:47:01 -0300

    feat: add cellphone

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -88,7 +88,7 @@ define([
             "messageTemplate": messageTemplate,
             "apiKey": apiKey,
             "email": "{{InteractionDefaults.Email}}",
-            "to": "{{Contact.Default.SMS}}" //<----This should map to your data extension name and phone number column
+            "to": "{{Event.EventKey.PhoneNumber}}" //<----This should map to your data extension name and phone number column
         }];
 
         payload['metaData'].isConfigured = true;
