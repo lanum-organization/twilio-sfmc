8b53c4811d401ffcdbe69a06624ea4b6bb3e5a10
Jonathan Tavares <jonathantavares.all@gmail.com>
2024-05-06 12:23:29 -0300

    teste 3

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -88,7 +88,7 @@ define([
             "messageTemplate": messageTemplate,
             "apiKey": apiKey,
             "email": "{{InteractionDefaults.Email}}",
-            "to": "{{EntrySource.PhoneNumber}}" //<----This should map to your data extension name and phone number column
+            "to": "{{InteractionDefaults.PhoneNumber}}" //<----This should map to your data extension name and phone number column
         }];
 
         payload['metaData'].isConfigured = true;
